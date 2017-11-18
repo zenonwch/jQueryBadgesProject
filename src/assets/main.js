e31632310c659dcee0e0b8eab21bb92d0a83254f
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-11-18 22:47:17 +0200

    All requirements completed

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,36 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/Zenonwch.json',
+    dataType: 'jsonp',
+    success: function(resp) {
+      addCourses(resp.courses.completed);
+    }
+  });
+
+  function addCourses(courses) {
+    let $badges = $('#badges');
+
+    courses.forEach(c => {
+      let $course = $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+
+      $('<h3 />', {
+        text: c.title
+      }).appendTo($course);
+
+      $('<img />', {
+        src: c.badge
+      }).appendTo($course);
+
+      $('<a />', {
+        href: c.url,
+        target: '_blank',
+        'class': 'btn btn-primary',
+        text: 'See Course'
+      }).appendTo($course);
+    });
+  }
 
 });
